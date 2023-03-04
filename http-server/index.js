bbf135625836dfabbcdffa15c6aacc6b4ed3f8e8
Abhishek Panday <125349848+abhishekpanday@users.noreply.github.com>
2023-03-04 22:44:54 +0530

    Create index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,18 +1,55 @@
-// Import  modules
-const http = require('http');
-const url = require('url');
-//  command line arguments
-const args = process.argv.slice(2);
-const port = args.includes('--port') ? args[args.indexOf('--port') + 1] : 3000;
-// Creation of HTTP server
-const server = http.createServer((req, res) => {
-const parsedUrl = url.parse(req.url, true);
-res.writeHead(300, { 'Content-Type': 'text/plain' });
-res.write(`Hello, your request ${parsedUrl.pathname}\n`);
-res.end();
-});
-//  specified port
-server.listen(port, () => {
-console.log(`Server is listening to port ${port}`);
+const http = require("http");
+
+const fs = require("fs");
+
+let args = require("minimist")(process.argv.slice(2), {
+  default: {
+    port: 5000,
+  },
 });
 
+let homeContent = "";
+let projectContent = "";
+let registerContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, register) => {
+  if (err) {
+    throw err;
+  }
+  registerContent = register;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHead(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registerContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(args.port);
